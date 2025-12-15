c3d353078a67c74ae2798285bbb1c67e8de249c4
Trubchaninov Andrii <112893271+Rey-ui@users.noreply.github.com>
2025-12-15 19:58:41 +0200

    add

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,21 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import { resolve } from 'path';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        // Абсолютные пути, чтобы Rollup видел файлы на сервере
-        input: glob.sync(resolve(__dirname, 'src/*.html')),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
+export default defineConfig({
+  // root: 'src', // убираем, потому что index.html в корне
+  build: {
+    outDir: 'dist', // собираем в dist в корне
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // путь к index.html в корне
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) return 'vendor';
         },
+        entryFileNames: '[name].js',
       },
-      outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload([resolve(__dirname, 'src/**/**.html')])],
-  };
+  },
+  plugins: [injectHTML(), FullReload([resolve(__dirname, '**/*.html')])],
 });
